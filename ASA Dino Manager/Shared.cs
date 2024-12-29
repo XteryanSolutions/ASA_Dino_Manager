04454622f0c1edfd780965aba97fdcaa33660d14
Martin <handuk420@gmail.com>
2024-12-29 02:02:19 +0200

    release build

--- ASA Dino Manager/Shared.cs
+++ ASA Dino Manager/Shared.cs
@@ -4,7 +4,7 @@
     internal class Shared
     {
         ////////////////////    Title version   ////////////////////
-        public static string version = "ASA Dino Manager 0.04.49";
+        public static string version = "ASA Dino Manager 0.04.50";
 
 
         ////////////////////////////////////////////////////////////
